3191791c44807a2f0f1cf9471bd3567f45580024
leocho12 <hjc021212@gmail.com>
2026-07-28 13:06:00 +0900

    add .gitignore and remove build files

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,58 +6,31 @@ using System.Threading.Tasks;
 //=========================================
 namespace ServerCore
 {
-    //메모리 배리어
-    //1. 코드 재배치 억제
-    //2. 가시성
-
-    //Full Memory Barrier: store, load 모두 억제
-    //Store Memory Barrier: store 억제, load 허용
-    //Load Memory Barrier: load 억제, store 허용
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
-        static void Thread1()
+        static void Thread_1()
         {
-            y = 1;//store y
-
-            //--------------------------------
-            Thread.MemoryBarrier();//메모리 배리어
-
-            r1 = x;//load x
+            for (int i = 0; i < 10000; i++)
+                number++;
         }
-
-        static void Thread2()
+        static void Thread_2()
         {
-            x = 1;//store x
-
-            //--------------------------------
-            Thread.MemoryBarrier();//메모리 배리어
-
-            r2 = y;//load y
+            for (int i = 0; i < 10000; i++)
+                number--;
         }
-
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-                Task t1=new Task(Thread1);
-                Task t2=new Task(Thread2);
-                t1.Start();
-                t2.Start();
+            Task t1=new Task(Thread_1);
+            Task t2=new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                Task.WaitAll(t1, t2);
+            Task.WaitAll(t1, t2);
 
-                if (r1 == 0 && r1 == 0)
-                    break;
-            }
-            Console.WriteLine($"{count}번 만에 빠져나옴");
+            Console.WriteLine(number);
+
         }
     }
 }
