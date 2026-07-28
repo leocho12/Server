3554c4257d76f25f7c054abe741b6fe0ceec137c
leocho12 <hjc021212@gmail.com>
2026-07-28 11:19:54 +0900

    Memory Barrier

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,31 +1,63 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-
+//=========================================
+//하드웨어 최적화
+//=========================================
 namespace ServerCore
 {
+    //메모리 배리어
+    //1. 코드 재배치 억제
+    //2. 가시성
+
+    //Full Memory Barrier: store, load 모두 억제
+    //Store Memory Barrier: store 억제, load 허용
+    //Load Memory Barrier: load 억제, store 허용
     class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread1()
+        {
+            y = 1;//store y
+
+            //--------------------------------
+            Thread.MemoryBarrier();//메모리 배리어
+
+            r1 = x;//load x
+        }
+
+        static void Thread2()
+        {
+            x = 1;//store x
+
+            //--------------------------------
+            Thread.MemoryBarrier();//메모리 배리어
+
+            r2 = y;//load y
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int i = 0; i < 10000; i++)
-                    for (int j = 0; j < 10000; j++)
-                        arr[i, j] = 1;
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"Time taken: {(end - now) / 10000} ms");
-            }
+                count++;
+                x = y = r1 = r2 = 0;
+                Task t1=new Task(Thread1);
+                Task t2=new Task(Thread2);
+                t1.Start();
+                t2.Start();
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int i = 0; i < 10000; i++)
-                    for (int j = 0; j < 10000; j++)
-                        arr[j, i] = 1;
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"Time taken: {(end - now) / 10000} ms");
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r1 == 0)
+                    break;
             }
+            Console.WriteLine($"{count}번 만에 빠져나옴");
         }
     }
 }
--- ServerCore/obj/Release/net10.0/ServerCore.AssemblyInfo.cs
+++ ServerCore/obj/Release/net10.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c00b0cc7ae96e20d0364fa58efc21f244896b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
