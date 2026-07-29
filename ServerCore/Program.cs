07c1bf15a406db0bd188300e36f3786ffb0ef15c
leocho12 <hjc021212@gmail.com>
2026-07-29 10:38:45 +0900

    Interlocked.Exchange로 교체

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -8,18 +8,25 @@ namespace ServerCore
 {
     class SpinLock
     {
-        volatile bool _locked = false;
+        volatile int _locked = 0;
         public void Acquire()//획득
         {
-            while (_locked)//잠금이 풀릴때까지 반복
-            {
+            //while (_locked)//잠금이 풀릴때까지 반복
+            //{
 
+            //}
+            //_locked = true;//잠금 획득
+            //위 코드는 잠금을 획득하는 동안 다른 스레드가 lock획득 가능해서 제대로 동작하지 않음
+            while(true)
+            {
+                int original = Interlocked.Exchange(ref _locked, 1);//원래 값을 original에 저장하고 _locked를 1로 바꿈
+                if (original == 0)//original이 0이면 다른 스레드가 잠금을 획득하지 않은 상태이므로 잠금 획득
+                    break;
             }
-            _locked = true;//잠금 획득
         }
         public void Release()//해제
         {
-
+            _locked = 0;
         }
     }
     class Program
