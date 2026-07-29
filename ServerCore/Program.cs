e51fbbae7e585caf3c8f68fab4dd0aae74e6e114
leocho12 <hjc021212@gmail.com>
2026-07-29 14:09:09 +0900

    Reader Writer Lock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,45 +2,42 @@
 using System.Threading;
 using System.Threading.Tasks;
 //=========================================
-//Event
+//Reader Writer Lock
 //=========================================
 namespace ServerCore
 {
     class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex();//커널동기화 객체여서 느림
-        //Mutex는 잠군 횟수,잠금 유무, 스레드 아이디를 확인할 수 있음. (재진입 가능) => 비용이 많이 든다
-
-        static void Thread_1()
+        static object _lock = new object();
+        class Reward
         {
-            for(int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
+
+        }
+        static ReaderWriterLockSlim _lock3=new ReaderWriterLockSlim();
+
+        //99.99%
+        static Reward GetRewardById(int id)
+        {
+            _lock3.EnterReadLock();
+
+            _lock3.ExitReadLock();
+
+            return null;
         }
 
-        static void Thread_2()
+        //0.01%
+        static void AddReward(Reward reward)
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
+            _lock3.EnterWriteLock();
+            _lock3.ExitWriteLock();
+            
         }
         static void Main(string[] args)
         {
-            Task t1=new Task(Thread_1);
-            Task t2=new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            lock (_lock)
+            {
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
+            }
         }
     }
 }
