62884eb9c0bafcf3e0b158880ce99afbe5f9fce4
leocho12 <hjc021212@gmail.com>
2026-07-30 11:12:52 +0900

    Thread local storage

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,42 +2,25 @@
 using System.Threading;
 using System.Threading.Tasks;
 //=========================================
-//Reader Writer Lock
+//Thread local storage
 //=========================================
 namespace ServerCore
 {
-    //재귀적 락을 허용할지(yes) writelock가진 상태에서 writelock을 가지는 건 허용/ readlock가진 상태에서 writelock을 가지는 건 불가
-    //스핀 락 정책(5000번 -> 양보)
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>();// 스레드마다 고유한 공간이 생김
 
+        static void WhoAmI()
+        {
+            ThreadName.Value = $"my name is {Thread.CurrentThread.ManagedThreadId}";
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine(ThreadName.Value);
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for(int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnlock();
-                }
-            });
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnlock();
-                }
-            });
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(count);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
         }
     }
 }
