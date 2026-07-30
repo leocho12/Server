3081a245cb969ba037dcdef8813c5804fde4f796
leocho12 <hjc021212@gmail.com>
2026-07-30 13:57:52 +0900

    Thread local storage

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -8,19 +8,24 @@ namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>();// 스레드마다 고유한 공간이 생김
+        //스레드 고유의 전역 변수를 만들고 싶을 때 ThreadLocal<T>를 사용
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"my name is {Thread.CurrentThread.ManagedThreadId}"; });// 이미 사용한 스레드를 재사용함
 
         static void WhoAmI()
         {
-            ThreadName.Value = $"my name is {Thread.CurrentThread.ManagedThreadId}";
-
-            Thread.Sleep(1000);
-
-            Console.WriteLine(ThreadName.Value);
+            bool repeat = ThreadName.IsValueCreated;
+            if(repeat)
+                Console.WriteLine(ThreadName.Value+"(repeat)");
+            else
+                Console.WriteLine(ThreadName.Value);
         }
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
             Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+
+            ThreadName.Dispose();
         }
     }
 }
