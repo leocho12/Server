fc6567c3d66dced9eba16b626bd3faa7b7577d5e
leocho12 <hjc021212@gmail.com>
2026-08-06 16:15:38 +0900

    서버 코어 코딩

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,31 +1,66 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 //=========================================
-//Thread local storage
+//Socket programming
 //=========================================
 namespace ServerCore
 {
     class Program
     {
-        //스레드 고유의 전역 변수를 만들고 싶을 때 ThreadLocal<T>를 사용
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"my name is {Thread.CurrentThread.ManagedThreadId}"; });// 이미 사용한 스레드를 재사용함
-
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated;
-            if(repeat)
-                Console.WriteLine(ThreadName.Value+"(repeat)");
-            else
-                Console.WriteLine(ThreadName.Value);
-        }
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            //DNS
 
-            ThreadName.Dispose();
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기의 무전기 생성
+
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try 
+            {
+                // 문지기의 주소 연동
+                listenSocket.Bind(endPoint);
+
+                //영업시작
+                //backlog : 대기열의 최대 길이
+                listenSocket.Listen(10);//대기열의 최대 길이 10
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    //손님입장
+                    Socket clientSocket = listenSocket.Accept();//클라이언트 소켓 생성
+
+                    //수신
+                    byte[] recvBuff = new byte[1024];//수신버퍼
+                    int recvBytes = clientSocket.Receive(recvBuff);//수신한 바이트 수
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);//수신한 데이터를 문자열로 변환
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    //전송
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("welcome to my server");//전송할 데이터를 바이트 배열로 변환
+                    clientSocket.Send(sendBuff);//전송
+
+                    //쫒아내기
+                    clientSocket.Shutdown(SocketShutdown.Both);//양방향 통신 종료
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            
         }
     }
 }
