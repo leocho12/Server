0cd3475d3fec06c82f0ea077d904c5c40af5a28f
leocho12 <hjc021212@gmail.com>
2026-08-06 16:24:19 +0900

    클라생성

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //DNS
+
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            //휴대폰 설정
+            Socket socket=new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //문지기에게 입장문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+
+                //전송
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                int sendBytes = socket.Send(sendBuff);
+
+
+                //수신
+                byte[] recvBuff = new byte[1024];
+                int socketBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, socketBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                //나가기
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                    Console.WriteLine(e.ToString);
+            }
+
+            
+        }
+    }
+}
