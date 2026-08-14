b2056ee0c01f1ae2e5e8466e9a9b8860e4121243
leocho12 <hjc021212@gmail.com>
2026-08-14 16:58:46 +0900

    _sendArgs를 재사용하기 위해 큐에 쌓았다가 전송하는 방식으로 변경

--- DummyClient/obj/Debug/net10.0/DummyClient.AssemblyInfo.cs
+++ DummyClient/obj/Debug/net10.0/DummyClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b794b1088eae7ec135c5d77f7020a2e410aa9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb8bfbdf700555c76c95d49c04c619969267e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DummyClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -15,6 +15,11 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;// 연결 상태 체크용 0=연결중, 1=끊김
 
+        object _lock = new object();// 큐에 데이터를 넣고 빼는 작업이 동시에 일어나면 문제가 생기므로 lock을 걸어줌
+        Queue<byte[]> _sendQueue=new Queue<byte[]>();// 전송할 데이터를 담을 큐
+        bool _pending = false;// 전송중인지 체크용
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();// 송신전용 소켓 생성
+
         public void Start(Socket socket)
         {
             _socket = socket;// 전달 받은 소켓 저장
@@ -29,12 +34,20 @@ namespace ServerCore
              */
             recvArgs.SetBuffer(new byte[1024], 0, 1024);// 데이터를 담을 버퍼 지정
 
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);// sendArgs객체의 Completed델리게이트에 OnSendCompleted 함수를 등록
+
+
             RegisterRecv(recvArgs);// 수신 대기 시작
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);// 동기전송 호출한 스레드가 전송 완료 될때 까지 정지
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pending == false)
+                    RegisterSend();
+            }
         }
 
         public void Disconnect()
@@ -48,9 +61,53 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
+
+        void RegisterSend()
+        {
+            // 이미 락을 건 상태에서 호출 함으로 락을 걸 필요 없음
+            _pending = true;
+            byte[] buff= _sendQueue.Dequeue();// 큐에서 데이터를 하나 꺼냄
+            _sendArgs.SetBuffer(buff,0, buff.Length);// 버퍼연결
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+                OnSendCompleted(null, _sendArgs);
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            // 콜백으로 호출된 경우 락이 필요
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)// 전송 성공
+                {
+                    // TODO
+                    try
+                    {
+                        if(_sendQueue.Count>0)
+                            RegisterSend();// 큐에 데이터가 남아있으면 다시 전송
+                        else
+                             _pending = false;
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else// 전송 실패
+                {
+                    Disconnect();// 연결 해제
+                }
+            }
+            
+        }
+
         void RegisterRecv(SocketAsyncEventArgs args)
         {
             bool pending = _socket.ReceiveAsync(args);// 비동기적으로 연결 요청을 받음
+            // 현재 이밴트가 하나밖에 없기때문에 완료될때마다 각기 다른 스레드에서 호출될수는 있어도 동시에 호출될 일은 없음
             if (pending == false)// 연결 즉시 잡혀서 이밴트가 불리지 않음 -> 직접 OnRevCompleted 호출
                 OnRevCompleted(null, args);
         }
@@ -73,7 +130,7 @@ namespace ServerCore
             }
             else
             {
-                // TODO : disconnect
+                Disconnect();// 연결 해제
 
             }
         }
